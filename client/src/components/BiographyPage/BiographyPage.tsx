5a8437cd54f43faa937d96a58011ee5189403514
Danyl-Dudka <danyad.stokes@gmail.com>
2025-12-16 21:39:58 -0500

    feat(ui): add adaptive design for medium/large screen

--- client/src/components/BiographyPage/BiographyPage.tsx
+++ client/src/components/BiographyPage/BiographyPage.tsx
@@ -19,8 +19,8 @@ export default function BiographyPage() {
             </div>
             <div className="owner_biography">
                 <p className='biography_white_description'>MY NAME IS <span className='second_part_description'>DANYL</span></p>
-                <p className='biography_white_description'>I’M UKRAINIAN <span className='yellow_part_description'>RUN</span><span className='second_part_word'>NER,</span></p>
-                <p className='biography_white_description'><span className='mentor_word_yellow'>MENTOR</span> AND <span className='second_part_club_word'>CLUB</span> <span className='owner_word_last_part'>OWNER.</span></p>
+                <p className='biography_white_description'>I’M UKRAINIAN <span className='second_row'><span className='yellow_part_description'>RUN</span><span className='second_part_word'>NER,</span></span></p>
+                <p className='biography_white_description'><span ><span className='yellow_word'>MENTOR</span> <span className='and_white_word'>AND</span></span><span><span className='second_part_club_word'>CLUB</span> <span className='owner_word_last_part'>OWNER.</span></span></p>
             </div>
         </div>
     )
